dde9ba9d817626f7f0c9f1f888b0228e5cded08e
Melvin "Dumbass" Shwuaner <83349478+MelvinShwuaner@users.noreply.github.com>
2025-05-07 18:15:46 +0300

    Add files via upload

---
+++ NIGGA MOD/Code/Main.cs
@@ -0,0 +1,25 @@
+using NCMS;
+using UnityEngine;
+using HarmonyLib;
+using System;
+namespace GodsAndPantheons
+{
+    [ModEntry]
+    class Main : MonoBehaviour
+    {
+        void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(NIGGAAAAA));
+        }
+    }
+    public class NIGGAAAAA
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(MusicBox), nameof(MusicBox.playSound), new Type[] { typeof(string), typeof(float), typeof(float), typeof(bool), typeof(bool) })]
+        [HarmonyPriority(Priority.High)]
+        static void NIGGA(ref string pSoundPath)
+        {
+            pSoundPath = "NIGGA";
+        }
+    }
+}
